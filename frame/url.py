90687f7b4cd5f35f0b51b6dee0a99434ab457d7c
jemenc <jemendezc@correo.udistrital.edu.co>
2020-01-29 14:03:24 -0500

    add url_body variable in url.py

--- frame/url.py
+++ frame/url.py
@@ -1,5 +1,8 @@
 from functools import wraps
+import urllib3
 from urllib.parse import urlparse
+import json
+import threading
 
 #decorator
 def url_verify(f):
@@ -8,39 +11,56 @@ def url_verify(f):
         if 'http' in args[0].url:
             return f(*args, **kwargs)
         else:
-            return f(URL('http://NaN/'))#raise RuntimeError('It is not a URL')
+            return f(URL('http://NaN/',npaths=10))#raise RuntimeError('It is not a URL')
     return decorator
 
 
 class URL:
-    """Class to extrac the domain name and url's path from an url"""
 
-
-    def __init__(self, url):
+    def __init__(self, url,**kwargs):
         self.url = url
-        self.domain, self.path = self.analyze_url()
+        if not kwargs:
+            kwargs = {"npaths": 10}
+        self.kwargs = kwargs
+        self.domain, self.path, self.subdomain, self.url_body  = self.analyze_url()
+
 
     @url_verify
     def analyze_url(self):
-        """
-        The funtion analize_url returns the domain name and the url's path from a url.
-        This function takes the inner variable url to extract the domain and url's path
-        There no need to spicifies an url format
-        """
-
+        npaths=self.kwargs["npaths"]
         obj_url = urlparse(self.url)
-        dom = obj_url.hostname
+        #sub = obj_url.hostname.rsplit('.', obj_url.hostname.count('.'))[0:1]
+        url_body = obj_url.hostname 
+        dom = obj_url.hostname.rsplit('.',obj_url.hostname.count('.'))[-2:]
+        if (len(dom[0])<4):
+            dom = obj_url.hostname.rsplit('.', obj_url.hostname.count('.'))[-3:]
+            sub=obj_url.hostname.replace('.'.join(dom),'')
+        dom='.'.join(dom)
+        sub = obj_url.hostname.replace('.'+dom, '')
         paths = obj_url.path.rsplit('/',obj_url.path.count('/'))[1:]
+        #self.url.replace(obj_url.hostname,'').replace('https','').replace('http','').replace('/','').replace(obj_url.path,'')
+        if npaths == 'default':
+            npaths=len(paths)
         if not paths:
             paths=["NaN"]
-        for i in range(0, 10):
+        for i in range(0, npaths):
             try:
                 if paths[i] == "":
                     paths[i]= "NaN"
             except:
-                paths.insert(i, "NaN")
-        return dom, paths
+                paths.insert (i, "NaN")
+        return dom, paths[0:npaths],sub,url_body
 
 
-#url = URL(url="http://json.parser.online.fr/")
-#url
+
+    def json(self):
+        return json.dumps({ "domain":self.domain, "paths":self.path[0:],"subdomain":self.subdomain, "url_body":self.url_body })
+
+
+
+
+a=URL("https://test.biblored.gov.co/",npaths='default')
+print(a.json())
+print(a.path)
+print(a.domain)
+print(a.subdomain)
