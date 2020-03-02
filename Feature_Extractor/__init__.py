276ea5ccab63cf991c2520b790e3b48af59793f3
dabarreiroh <dabarreiroh@unal.edu.co>
2020-03-02 16:36:48 -0500

    refatcturation

--- Feature_Extractor/__init__.py
+++ Feature_Extractor/__init__.py
@@ -55,6 +55,10 @@ class main():
         tld2=['biz','name','pro']
         """Dominios gTLD patrocinados"""
         tld3=['aero','asia','cat',	'coop','edu','gov','int','jobs','mil','mobi','museum','post','tel','travel','xxx']
+        """Dominios gTLD paises"""
+        tld4=['ac','ad','ae','af','ag','ai','al','am','ao','aq','ar','as','at','au','aw','ax','az','ba','bb','bd','be','bf','bg','bh','bi','bj','bm','bn','bo','br','bs','bt','bw','by','bz','ca','cc','cd','cf','cg','ch','ci','ck','cl','cm','cn','co','cr','cu','cv','cw','cx','cy','cz','de','dj','dk','dm','do','dz','ec','ee','eg','er','es','et','eu','fi','fj','fk','fm','fo','fr','ga','gd','ge','gf','gg','gh','gi','gl','gm','gn','gp','gq','gr','gs','gt','gu','gw','gy','hk','hm','hn','hr','ht','hu','id','ie','il','im','in','io','iq','ir','is','it','je','jm','jo','jp','ke','kg','kh','ki','km','kn','kp','kr','kw','ky','kz','la','lb','lc','li','lk','lr','ls','lt','lu','lv','ly','ma','mc','md','me','mg','mh','mk','ml','mm','mn','mo','mp','mq','mr','ms','mt','mu','mv','mw','mx','my','mz','na','nc','ne','nf','ng','ni','nl','no','np','nr','nu','nz','om','pa','pe','pf','pg','ph','pk','pl','pm','pn','pr','ps','pt','pw','py','qa','re','ro','rs','ru','rw','sa','sb','sc','sd','se','sg','sh','si','sk','sl','sm','sn','so','sr','st','su','sv','sx','sy','sz','tc','td','tf','tg','th','tj','tk','tl','tm','tn','to','tr','tt','tv','tw','tz','ua','ug','uk','us','uy','uz','va','vc','ve','vg','vi','vn','vu','wf','ws','ye','yt','za','zm','zw']
+
+
         tlds=lambda x: 1 if x.rsplit('.')[1] in str(tld) else 2 if x.domain.rsplit('.')[1] in str(tld2) else 3 if x.domain.rsplit('.')[1] in str(tld3) else 4
 
         dicc1 = json.dumps({"url description": {"url_len": url(urlw.url), "domain_len": domains(urlw.domain), "subdomain_len": subdomains(urlw.subdomain),"tld":tlds(urlw.domain),"special_chrtrs_count": pathschar(urlw.path) ,"paths_len": paths(urlw.path) }})
